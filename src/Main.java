92005169e2a71dc9de5cb63f2ec1febab065ae84
Petr Kim <ppeximo@gmail.com>
2023-02-13 17:11:55 +0300

    Main and Person code mogifid

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Queue<Person> attractionQueue = generateClients();
+
+        while (true) {
+            Person p = attractionQueue.poll();
+            if (p == null) break;                           // Проверка на наличие следующего элемента в очереди
+            if (p.ticketsAmount == 0) continue;             // Есть ли ещё билеты у посетителя
+                p.ticketsAmount--;
+                System.out.println("Прокатился на аттракционе " + p);
+                attractionQueue.offer(p);                   // Добавление в очередь, если остались билеты
+            }
+        }
+
+    public static LinkedList<Person> generateClients() {
+
+        LinkedList<Person> attrQueue = new LinkedList<>();
+
+        attrQueue.offer(new Person("Stive", "Smith", 4));
+        attrQueue.offer(new Person("Toto", "Tilt", 3));
+        attrQueue.offer(new Person("Omar", "Ruby", 6));
+        attrQueue.offer(new Person("Ingrid", "Smidt", 7));
+        attrQueue.offer(new Person("Ivan", "Kalita", 9));
+
+        return attrQueue;
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,17 @@
-public class Person {
+public  class Person {
+
+    public String name;
+    public String sureName;
+    public int ticketsAmount;
+
+    public Person(String name, String sureName, int ticketsAmount){
+        this.name = name;
+        this.sureName = sureName;
+        this.ticketsAmount = ticketsAmount;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + sureName + " Осталось: " + ticketsAmount +" белета(ов).";
+    }
 }
